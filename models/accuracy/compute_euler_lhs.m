de6e43211edcd78407a793ec982bef80da45b1d6
Stéphane Adjemian (Ryûk) <stepan@adjemian.eu>
2025-08-25 11:11:45 +0200

    Add doc header.

--- models/accuracy/compute_euler_lhs.m
+++ models/accuracy/compute_euler_lhs.m
@@ -1,6 +1,12 @@
 function ts = compute_euler_lhs(filename)
 
 % Return a dseries object with all the endogenous variables and the LHS of the Euler equation.
+%
+% INPUTS
+% - filename    [char]     1×n array, name of the mat file where a dseries containing the simulations for the endogenous variables are saved.
+%
+% OUTPUTS
+% - ts          [dseries]  updated dseries object with the left hand side of the Euler equation.
 
     ts = dseries(filename);
 
