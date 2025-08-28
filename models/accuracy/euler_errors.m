10734f8024b5e75a243b0b5c8aa7d81b9755e214
Stéphane Adjemian (Ryûk) <stepan@adjemian.eu>
2025-08-28 16:36:12 +0200

    Add Euler error evatuations.

---
+++ models/accuracy/euler_errors.m
@@ -0,0 +1,163 @@
+dynare rbcii_euler_errors
+
+ts_0 = compute_euler_residuals('rbcii-010-sep-0-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_0.save('euler-010-sep-0-algo-1-hybrid-0');
+
+
+ts_1_0 = compute_euler_residuals('rbcii-010-sep-1-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_1_0.save('euler-010-sep-1-algo-1-hybrid-0');
+
+
+ts_1_2 = compute_euler_residuals('rbcii-010-sep-1-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_1_2.save('euler-010-sep-1-algo-1-hybrid-2')
+
+
+ts_1_4 = compute_euler_residuals('rbcii-010-sep-1-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_1_4.save('euler-010-sep-1-algo-1-hybrid-4')
+
+
+ts_2_0 = compute_euler_residuals('rbcii-010-sep-2-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_2_0.save('euler-010-sep-2-algo-1-hybrid-0');
+
+
+ts_2_2 = compute_euler_residuals('rbcii-010-sep-2-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_2_2.save('euler-010-sep-2-algo-1-hybrid-2');
+
+
+ts_2_4 = compute_euler_residuals('rbcii-010-sep-2-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_2_4.save('euler-010-sep-2-algo-1-hybrid-4');
+
+
+
+ts_5_2 = compute_euler_residuals('rbcii-010-sep-5-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+ts_5_2.save('euler-010-sep-5-algo-1-hybrid-2');
+
+
+
+
+ts_0 = compute_euler_residuals('rbcii-050-sep-0-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_0.save('euler-050-sep-0-algo-1-hybrid-0');
+
+
+ts_1_0 = compute_euler_residuals('rbcii-050-sep-1-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_1_0.save('euler-050-sep-1-algo-1-hybrid-0');
+
+
+ts_1_2 = compute_euler_residuals('rbcii-050-sep-1-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_1_2.save('euler-050-sep-1-algo-1-hybrid-2')
+
+
+ts_1_4 = compute_euler_residuals('rbcii-050-sep-1-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_1_4.save('euler-050-sep-1-algo-1-hybrid-4')
+
+
+ts_2_0 = compute_euler_residuals('rbcii-050-sep-2-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_2_0.save('euler-050-sep-2-algo-1-hybrid-0');
+
+
+ts_2_2 = compute_euler_residuals('rbcii-050-sep-2-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_2_2.save('euler-050-sep-2-algo-1-hybrid-2');
+
+
+ts_2_4 = compute_euler_residuals('rbcii-050-sep-2-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_2_4.save('euler-050-sep-2-algo-1-hybrid-4');
+
+
+ts_5_2 = compute_euler_residuals('rbcii-050-sep-5-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_5_2.save('euler-050-sep-5-algo-1-hybrid-2');
+
+
+
+
+ts_0 = compute_euler_residuals('rbcii-100-sep-0-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_0.save('euler-100-sep-0-algo-1-hybrid-0');
+
+
+ts_1_0 = compute_euler_residuals('rbcii-100-sep-1-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_1_0.save('euler-100-sep-1-algo-1-hybrid-0');
+
+
+ts_1_2 = compute_euler_residuals('rbcii-100-sep-1-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_1_2.save('euler-100-sep-1-algo-1-hybrid-2')
+
+
+ts_1_4 = compute_euler_residuals('rbcii-100-sep-1-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_1_4.save('euler-100-sep-1-algo-1-hybrid-4')
+
+
+ts_2_0 = compute_euler_residuals('rbcii-100-sep-2-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_2_0.save('euler-100-sep-2-algo-1-hybrid-0');
+
+
+ts_2_2 = compute_euler_residuals('rbcii-100-sep-2-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_2_2.save('euler-100-sep-2-algo-1-hybrid-2');
+
+ts_2_4 = compute_euler_residuals('rbcii-100-sep-2-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_2_4.save('euler-100-sep-2-algo-1-hybrid-4');
+
+ts_5_2 = compute_euler_residuals('rbcii-100-sep-5-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_5_2.save('euler-100-sep-5-algo-1-hybrid-2');
+
+
+
+ts_0 = compute_euler_residuals('rbcii-005-sep-0-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_0.save('euler-005-sep-0-algo-1-hybrid-0');
+
+
+ts_1_0 = compute_euler_residuals('rbcii-005-sep-1-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_1_0.save('euler-005-sep-1-algo-1-hybrid-0');
+
+
+ts_1_2 = compute_euler_residuals('rbcii-005-sep-1-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_1_2.save('euler-005-sep-1-algo-1-hybrid-2')
+
+
+ts_1_4 = compute_euler_residuals('rbcii-005-sep-1-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_1_4.save('euler-005-sep-1-algo-1-hybrid-4')
+
+
+ts_2_0 = compute_euler_residuals('rbcii-005-sep-2-algo-1-hybrid-0.mat', 3, options_, M_, oo_);
+
+ts_2_0.save('euler-005-sep-2-algo-1-hybrid-0');
+
+
+ts_2_2 = compute_euler_residuals('rbcii-005-sep-2-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_2_2.save('euler-005-sep-2-algo-1-hybrid-2');
+
+
+ts_2_4 = compute_euler_residuals('rbcii-005-sep-2-algo-1-hybrid-4.mat', 3, options_, M_, oo_);
+
+ts_2_4.save('euler-005-sep-2-algo-1-hybrid-4');
+
+
+ts_5_2 = compute_euler_residuals('rbcii-005-sep-5-algo-1-hybrid-2.mat', 3, options_, M_, oo_);
+
+ts_5_2.save('euler-005-sep-5-algo-1-hybrid-2');
