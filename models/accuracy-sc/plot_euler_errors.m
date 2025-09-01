d88bd74f1264e05ef9b5ef1c1f305c549be0f189
Stéphane Adjemian (Argos) <stepan@adjemian.eu>
2025-09-01 16:51:56 +0200

    Change plots for accuracy measures.
    Remove SEP(2) with full tree... Which is dominated by SEP(5) with
    sparse tree. It's always easier to solve a problem with a sparse tree.

--- models/accuracy-sc/plot_euler_errors.m
+++ models/accuracy-sc/plot_euler_errors.m
@@ -2,6 +2,8 @@ ts0_nozlb = dseries('euler-007-sep-0-algo-1-hybrid-0.nozlb.mat');
 ts1_nozlb = dseries('euler-007-sep-1-algo-1-hybrid-0.nozlb.mat');
 ts2_nozlb = dseries('euler-007-sep-2-algo-1-hybrid-0.nozlb.mat');
 ts2_full_nozlb = dseries('euler-007-sep-2-algo-0-hybrid-0.nozlb.mat');
+ts5_nozlb = dseries('euler-007-sep-5-algo-1-hybrid-0.nozlb.mat');
+
 
 skipline(2)
 dprintf('NO-ZLB, Without pertubation based correction')
@@ -10,23 +12,27 @@ dprintf('average error: %f', mean(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))
 dprintf('average error: %f', mean(log10(abs(ts1_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_full_nozlb.EulerErrors_c.data(2:end))/100)))
+dprintf('average error: %f', mean(log10(abs(ts5_nozlb.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('max error: %f', max(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts1_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_full_nozlb.EulerErrors_c.data(2:end))/100)))
+dprintf('max error: %f', max(log10(abs(ts5_nozlb.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('min error: %f', min(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts1_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_full_nozlb.EulerErrors_c.data(2:end))/100)))
+dprintf('min error: %f', min(log10(abs(ts5_nozlb.EulerErrors_c.data(2:end))/100)))
 
 figure(1)
 plot(ts0_nozlb.Investment.data(2:end), log10(abs(ts0_nozlb.EulerErrors_c.data(2:end)/100)), '.k')
 hold on
 plot(ts1_nozlb.Investment.data(2:end), log10(abs(ts1_nozlb.EulerErrors_c.data(2:end)/100)), '.b')
 plot(ts2_nozlb.Investment.data(2:end), log10(abs(ts2_nozlb.EulerErrors_c.data(2:end)/100)), '.g')
-plot(ts2_full_nozlb.Investment.data(2:end), log10(abs(ts2_full_nozlb.EulerErrors_c.data(2:end)/100)), '.r')
+plot(ts5_nozlb.Investment.data(2:end), log10(abs(ts5_nozlb.EulerErrors_c.data(2:end)/100)), '.r')
+% plot(ts2_full_nozlb.Investment.data(2:end), log10(abs(ts2_full_nozlb.EulerErrors_c.data(2:end)/100)), '.r')
 hold off
 axis tight
 box on
@@ -38,6 +44,7 @@ matlab2tikz( 'rbcii_euler_nozlb.tikz' );
 
 ts1_nozlb_hybrid = dseries('euler-007-sep-1-algo-1-hybrid-4.nozlb.mat');
 ts2_nozlb_hybrid = dseries('euler-007-sep-2-algo-1-hybrid-4.nozlb.mat');
+ts5_nozlb_hybrid = dseries('euler-007-sep-5-algo-1-hybrid-4.nozlb.mat');
 ts2_full_nozlb_hybrid = dseries('euler-007-sep-2-algo-0-hybrid-4.nozlb.mat');
 
 skipline(2)
@@ -47,23 +54,27 @@ dprintf('average error: %f', mean(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))
 dprintf('average error: %f', mean(log10(abs(ts1_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_full_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('average error: %f', mean(log10(abs(ts5_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('max error: %f', max(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts1_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_full_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('max error: %f', max(log10(abs(ts5_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('min error: %f', min(log10(abs(ts0_nozlb.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts1_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_full_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('min error: %f', min(log10(abs(ts5_nozlb_hybrid.EulerErrors_c.data(2:end))/100)))
 
 figure(2)
 plot(ts0_nozlb.Investment.data(2:end), log10(abs(ts0_nozlb.EulerErrors_c.data(2:end)/100)), '.k')
 hold on
 plot(ts1_nozlb_hybrid.Investment.data(2:end), log10(abs(ts1_nozlb_hybrid.EulerErrors_c.data(2:end)/100)), '.b')
 plot(ts2_nozlb_hybrid.Investment.data(2:end), log10(abs(ts2_nozlb_hybrid.EulerErrors_c.data(2:end)/100)), '.g')
-plot(ts2_full_nozlb_hybrid.Investment.data(2:end), log10(abs(ts2_full_nozlb_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
+plot(ts5_nozlb_hybrid.Investment.data(2:end), log10(abs(ts2_nozlb_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
+%plot(ts2_full_nozlb_hybrid.Investment.data(2:end), log10(abs(ts2_full_nozlb_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
 hold off
 axis tight
 box on
@@ -76,6 +87,7 @@ matlab2tikz( 'rbcii_euler_nozlb_hybrid.tikz' );
 ts0 = dseries('euler-007-sep-0-algo-1-hybrid-0.mat');
 ts1 = dseries('euler-007-sep-1-algo-1-hybrid-0.mat');
 ts2 = dseries('euler-007-sep-2-algo-1-hybrid-0.mat');
+ts5 = dseries('euler-007-sep-5-algo-1-hybrid-0.mat');
 ts2_full = dseries('euler-007-sep-2-algo-0-hybrid-0.mat');
 
 skipline(2)
@@ -85,23 +97,27 @@ dprintf('average error: %f', mean(log10(abs(ts0.EulerErrors_c.data(2:end))/100))
 dprintf('average error: %f', mean(log10(abs(ts1.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_full.EulerErrors_c.data(2:end))/100)))
+dprintf('average error: %f', mean(log10(abs(ts5.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('max error: %f', max(log10(abs(ts0.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts1.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_full.EulerErrors_c.data(2:end))/100)))
+dprintf('max error: %f', max(log10(abs(ts5.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('min error: %f', min(log10(abs(ts0.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts1.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_full.EulerErrors_c.data(2:end))/100)))
+dprintf('min error: %f', min(log10(abs(ts5.EulerErrors_c.data(2:end))/100)))
 
 figure(3)
 plot(ts0.Investment.data(2:end), log10(abs(ts0.EulerErrors_c.data(2:end)/100)), '.k')
 hold on
 plot(ts1.Investment.data(2:end), log10(abs(ts1.EulerErrors_c.data(2:end)/100)), '.b')
 plot(ts2.Investment.data(2:end), log10(abs(ts2.EulerErrors_c.data(2:end)/100)), '.g')
-plot(ts2_full.Investment.data(2:end), log10(abs(ts2_full.EulerErrors_c.data(2:end)/100)), '.r')
+plot(ts5.Investment.data(2:end), log10(abs(ts5.EulerErrors_c.data(2:end)/100)), '.r')
+%plot(ts2_full.Investment.data(2:end), log10(abs(ts2_full.EulerErrors_c.data(2:end)/100)), '.r')
 hold off
 axis tight
 box on
@@ -113,6 +129,8 @@ matlab2tikz( 'rbcii_euler.tikz' );
 
 ts1_hybrid = dseries('euler-007-sep-1-algo-1-hybrid-4.mat');
 ts2_hybrid = dseries('euler-007-sep-2-algo-1-hybrid-4.mat');
+ts2_hybrid = dseries('euler-007-sep-2-algo-1-hybrid-4.mat');
+ts5_hybrid = dseries('euler-007-sep-5-algo-1-hybrid-4.mat');
 ts2_full_hybrid = dseries('euler-007-sep-2-algo-0-hybrid-4.mat');
 
 skipline(2)
@@ -122,23 +140,27 @@ dprintf('average error: %f', mean(log10(abs(ts0.EulerErrors_c.data(2:end))/100))
 dprintf('average error: %f', mean(log10(abs(ts1_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('average error: %f', mean(log10(abs(ts2_full_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('average error: %f', mean(log10(abs(ts5_hybrid.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('max error: %f', max(log10(abs(ts0.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts1_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('max error: %f', max(log10(abs(ts2_full_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('max error: %f', max(log10(abs(ts5_hybrid.EulerErrors_c.data(2:end))/100)))
 skipline()
 dprintf('min error: %f', min(log10(abs(ts0.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts1_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_hybrid.EulerErrors_c.data(2:end))/100)))
 dprintf('min error: %f', min(log10(abs(ts2_full_hybrid.EulerErrors_c.data(2:end))/100)))
+dprintf('min error: %f', min(log10(abs(ts5_hybrid.EulerErrors_c.data(2:end))/100)))
 
 figure(4)
 plot(ts0.Investment.data(2:end), log10(abs(ts0.EulerErrors_c.data(2:end)/100)), '.k')
 hold on
 plot(ts1_hybrid.Investment.data(2:end), log10(abs(ts1_hybrid.EulerErrors_c.data(2:end)/100)), '.b')
 plot(ts2_hybrid.Investment.data(2:end), log10(abs(ts2_hybrid.EulerErrors_c.data(2:end)/100)), '.g')
-plot(ts2_full_hybrid.Investment.data(2:end), log10(abs(ts2_full_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
+plot(ts5_hybrid.Investment.data(2:end), log10(abs(ts5_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
+%plot(ts2_full_hybrid.Investment.data(2:end), log10(abs(ts2_full_hybrid.EulerErrors_c.data(2:end)/100)), '.r')
 hold off
 axis tight
 box on
